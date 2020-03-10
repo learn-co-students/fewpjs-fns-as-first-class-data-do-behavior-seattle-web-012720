2cb2b86ef9ef2afe26bfe0b04201e2cb4c0f05ca
davidka7 <david.perekopskiy@go.shoreline.edu>
2020-03-10 13:41:17 -0700

    Done.

--- index.js
+++ index.js
@@ -6,6 +6,30 @@ function handleClick(e) {
 }
 
 /* End Given Code, don't edit above here...*/
+function greet(time) {
+  const hours = time.split(":")
+if (parseInt(hours[0]) < 12)
+   {
 
+      return 'Good Morning'
+}
+else if (parseInt(hours[0]) >= 12 && parseInt(hours[0]) <= 16)
+  {
+    
+
+      return 'Good Afternoon'
+  }
+else if (parseInt(hours[0]) >= 17)
+  {
+    
+      return 'Good Evening'
+}
+
+
+}
+function displayMessage(string) {
+  const node = document.getElementById('greeting')
+  node.innerText = string
+}
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
