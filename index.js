6c01dd5187dfbe399462e0b3c1feb3694fa3e145
kailanak1 <kailanakahawaii@gmail.com>
2020-03-09 19:39:02 -0700

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet (string){
+  let num = parseInt(string, 10)
+  if (num < 12) { 
+    return "Good Morning"
+  }
+  else if (num < 17){ 
+    return "Good Afternoon"
+  } else { 
+    return "Good Evening"
+  }
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(string){
+  let input = document.getElementById("greeting");
+  input.innerText = string 
+
+}
